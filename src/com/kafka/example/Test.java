e0bcc20e1068181d5f785836b4edac288c0813da
unknown <viewprasad7@gmail.com>
2020-05-14 16:13:24 +0530

    chenges

--- src/com/kafka/example/Test.java
+++ src/com/kafka/example/Test.java
@@ -3,10 +3,9 @@ package com.kafka.example;
 import java.util.ArrayList;
 
 public class Test {
-	public  void main(String args) {
+	public static void main(String args) {
 		System.out.println("test");
 	}
-
 	public static void main(String[] args) {
 		try {
 
